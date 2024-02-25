573c392f627e9bbc44a2e7da0c19b3339a13bd54
Nurislom3000 <nurislomnavdar1@gmail.com>
2024-02-25 11:57:57 +0500

    bg

--- vue-project/src/components/pagesComponents/main/Intro.vue
+++ vue-project/src/components/pagesComponents/main/Intro.vue
@@ -37,7 +37,7 @@ export default {}
 .Intro {
 	width: 100vw;
 	height: 701px;
-	background: url('../../../../public/intro.jpg');
+	background: url('../../../../public/Intro.jpg');
 	background-size: 100% 100%;
 	background-repeat: no-repeat;
 	background-color: rgba(0, 0, 0, 0.27);
